66bb7618ef217e9d227447712a76d79fb269244d
Andyiev <ayevdokymenmk@gmail.com>
2021-07-07 22:03:37 -0600

    Add displaying appointments

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,15 +1,14 @@
-import React from "react";
-
+import React from "react"
 import "components/Appointment/styles.scss";
+import Header from "components/Appointment/Header"
+import Empty from "components/Appointment/Empty"
+import Show from "components/Appointment/Show";
 
 
-const Appointment = function() {
+export default function Appointment(props) {
   return (
-    <arcticle className="appointment"></arcticle>
-  )
-}
-
-
-
-
-export default Appointment;
+    <article className="appointment"> 
+    <Header time= {props.time} />
+    {props.interview ? <Show student= {props.interview.student} interviewer= {props.interview.interviewer} /> : <Empty />}
+    </article>
+  )}
--- stories/index.js
+++ stories/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
@@ -159,14 +159,20 @@ storiesOf("InterviewerListItem", module)
     onClose={action("onClose")}
   />)
   .add("Edit", () => <Form 
-    name= "Mandy"
-    interviewers= {interviewers}
-    interviewer={3}
-    onSave= {action("onSave")}
-    onCancel= {action("onCancel")}
+    name="Mandy"
+    interviewers={interviewers}
+    onSave={action("onSave")}
+    onCancel={action("onCancel")}
   />)
   .add("Create", () => <Form
     interviewers={interviewers}
     onSave= {action("onSave")}
     onCancel= {action("onCancel")}
   />)
+  .add("Appointment Empty", () => (
+    <Fragment>
+      <Appointment id={1} time="12pm" />
+      <Appointment id="last" time="1pm" />
+    </Fragment>
+  ))
+  
