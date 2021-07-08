eed70080f9442f1af2091884287446f4c7893fcb
Andyiev <ayevdokymenmk@gmail.com>
2021-07-08 15:01:23 -0600

    Add days request

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,8 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import "components/Application.scss";
 import DayList from "components/DayList";
 import Appointment from "components/Appointment";
+import axios from "axios";
 
 const appointments = [
   {
@@ -53,23 +54,16 @@ const appointments = [
 
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
-  const days = [
-    {
-      id: 1,
-      name: "Monday",
-      spots: 2,
-    },
-    {
-      id: 2,
-      name: "Tuesday",
-      spots: 5,
-    },
-    {
-      id: 3,
-      name: "Wednesday",
-      spots: 0,
-    },
-  ];
+  const [days, setDays] = useState([]);
+  useEffect(() => {
+    axios.get('/api/days')
+    .then(response => {
+      setDays(response.data)
+    });
+}, [])
+
+
+  
 
   return (
     <main className="layout">
