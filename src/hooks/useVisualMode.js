bec7f4b1eb7fcdf1876f763de0d8aa45c397ea8b
Andyiev <ayevdokymenmk@gmail.com>
2021-07-08 21:50:44 -0600

    Add back, back limit, transition with replace

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,9 +1,25 @@
 import { useState } from "react";
 
-export default function useVisualMode(initialMode) {
-  const [mode, setMode] = useState(initialMode);
+export default function useVisualMode(initial) {
+  // initialize state
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
 
-  const transition = (mode) => setMode(mode);
+  function transition(mode, replace = false) {
+    if (replace) {
+      setHistory(prev => [...prev.slice(0, prev.length - 1), mode]);
+    } else {
+      setHistory(prev => [...prev, mode]);
+    }
+    setMode(mode);
+  }
 
-  return { mode, transition }
-} 
+  function back() {
+    if (history.length > 1) {
+      setHistory(prev => [...prev.slice(0, prev.length - 1)]);
+      setMode(history[history.length - 2]);
+    }
+  }
+
+  return { mode, transition, back };
+}
